99282576ef8a00e0d3f8146d04bd5914e84f275f
Ryan Ayoubi <rsaub3@gmail.com>
2023-10-07 18:02:16 -0700

    added rooms

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const socket = new WebSocket('ws://localhost:3000');
 
+// Send a request for available rooms when the page loads
 socket.onopen = () => {
-  // Send a request for online users when the WebSocket connection is established
-  socket.send(JSON.stringify({ request: 'getUsers' }));
+  socket.send(JSON.stringify({ request: 'getRooms' }));
 };
 
 socket.onmessage = (event) => {
   const data = JSON.parse(event.data);
 
-  // Check if the response is for online users
-  if (data.users) {
-    const onlineUsersList = document.getElementById('onlineUsers');
-    onlineUsersList.innerHTML = '';
-    data.users.forEach((user) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = user;
-      onlineUsersList.appendChild(listItem);
+  // Check if the response is for available rooms
+  if (data.rooms) {
+    const roomList = document.getElementById('roomList');
+    roomList.innerHTML = '';
+    data.rooms.forEach((room) => {
+      const roomItem = document.createElement('li');
+      roomItem.textContent = room;
+      roomItem.onclick = () => joinRoom(room);
+      roomList.appendChild(roomItem);
     });
+  } else if (data.message) {
+    // Handle success messages
+    console.log(data.message);
   } else if (data.error) {
-    // Handle errors, if any
+    // Handle error messages
     console.error(data.error);
+  } else if (data.users) {
+    // Update the room title and online users when receiving user data for a specific room
+    updateRoomInfo(data.room, data.users);
   }
 };
 
@@ -33,5 +40,38 @@ function setUsername() {
     socket.send(JSON.stringify(data));
     usernameInput.value = '';
   }
+}
+
+function getRooms(){
+  socket.send(JSON.stringify({ request: 'getRooms' }));
+}
+
+function joinRoom(room) {
+  socket.send(JSON.stringify({ request: 'joinRoom', room: room }));
+}
+
+
+function createRoom() {
+  const roomName = prompt('Enter the new room name:');
+  if (roomName) {
+    socket.send(JSON.stringify({ request: 'createRoom', room: roomName }));
+    
+    // Request the updated list of rooms after creating a new room
+    socket.send(JSON.stringify({ request: 'getRooms' }));
+  }
+}
+
+
+function updateRoomInfo(room, users) {
+  const roomTitle = document.getElementById('roomTitle');
+  const onlineUsersList = document.getElementById('onlineUsers');
+
+  roomTitle.textContent = `Room: ${room}`;
+  onlineUsersList.innerHTML = '';
+  users.forEach((user) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = user;
+    onlineUsersList.appendChild(listItem);
+  });
 }
 
--- server.js
+++ server.js
@@ -6,49 +6,104 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const onlineUsers = new Map(); // Using a Map to store username and corresponding WebSocket instance
+const rooms = new Map(); // Using a Map to store room names and their online users
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     const data = JSON.parse(message);
 
-    if (data.request === 'getUsers') {
-      // If the client requests the list of online users, send it to them
-      const users = Array.from(onlineUsers.keys());
-      ws.send(JSON.stringify({ users: users }));
+    if (data.request === 'getRooms') {
+      // If the client requests the list of available rooms, send it to them
+      const roomList = Array.from(rooms.keys());
+      ws.send(JSON.stringify({ rooms: roomList }));
+    } else if (data.request === 'joinRoom') {
+      // Handle joining a room
+      const roomToJoin = data.room;
+      // Remove the user from their current room, if any
+      leaveCurrentRoom(ws);
+      // Add the user to the selected room
+      joinRoom(roomToJoin, ws);
+
+      // Send the updated list of online users for the selected room
+      broadcastOnlineUsers(roomToJoin);
+    } else if (data.request === 'createRoom') {
+      // Handle creating a new room
+      const newRoomName = data.room;
+      if (!rooms.has(newRoomName)) {
+        rooms.set(newRoomName, []);
+        ws.send(JSON.stringify({ message: `Room "${newRoomName}" created successfully!` }));
+      } else {
+        ws.send(JSON.stringify({ error: `Room "${newRoomName}" already exists.` }));
+      }
     } else if (data.username) {
       // Handle setting and updating usernames
       const newUsername = data.username;
 
       // Check if the new username is already in use
-      if (onlineUsers.has(newUsername)) {
+      if (ws.username !== newUsername && isUsernameInUse(newUsername, ws.room)) {
         // If the new username is in use, send a message to the client indicating the conflict
         ws.send(JSON.stringify({ error: 'Username is already in use' }));
       } else {
         // If the new username is not in use, remove the old username and set the new one
-        onlineUsers.delete(ws.username);
         ws.username = newUsername;
-        onlineUsers.set(newUsername, ws);
         
-        // Broadcast the updated list of online users to all clients
-        broadcastOnlineUsers();
+        // Broadcast the updated list of online users for the room to all clients
+        broadcastOnlineUsers(ws.room);
       }
     }
   });
 
   ws.on('close', () => {
-    // Remove the user from the onlineUsers map when they disconnect
-    onlineUsers.delete(ws.username);
-    // Broadcast the updated list of online users to all clients
-    broadcastOnlineUsers();
+    // Remove the user from their current room when they disconnect
+    leaveCurrentRoom(ws);
   });
 });
 
-function broadcastOnlineUsers() {
-  const users = Array.from(onlineUsers.keys());
-  wss.clients.forEach((client) => {
+function isUsernameInUse(username, room) {
+  if (!room) return false; // No room selected
+
+  const roomUsers = rooms.get(room);
+  if (!roomUsers) return false; // Room doesn't exist
+
+  return roomUsers.some((user) => user.username === username);
+}
+
+function joinRoom(room, ws) {
+  let roomUsers = rooms.get(room);
+
+  if (!roomUsers) {
+    roomUsers = [];
+    rooms.set(room, roomUsers);
+  }
+
+  roomUsers.push(ws);
+  ws.room = room;
+}
+
+function leaveCurrentRoom(ws) {
+  if (ws.room) {
+    const roomUsers = rooms.get(ws.room);
+
+    if (roomUsers) {
+      const index = roomUsers.indexOf(ws);
+      if (index !== -1) {
+        roomUsers.splice(index, 1);
+        broadcastOnlineUsers(ws.room);
+        ws.room = null;
+      }
+    }
+  }
+}
+
+function broadcastOnlineUsers(room) {
+  const roomUsers = rooms.get(room);
+  if (!roomUsers) return;
+
+  const users = roomUsers.map((user) => user.username);
+  
+  roomUsers.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ users: users }));
+      client.send(JSON.stringify({ users: users, room: room }));
     }
   });
 }
