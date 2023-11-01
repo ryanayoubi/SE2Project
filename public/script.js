f4e7767b85081f40c45dbfa7927d6e2e2c94aa2c
Prem Agarwal <prem.m.agarwal@gmail.com>
2023-11-01 14:57:55 -0700

    implemented chats
    only 1 character messages clearing up bugs

---
+++ public/script.js
@@ -0,0 +1,114 @@
+const socket = new WebSocket('ws://localhost:3000');
+let currentRoom = null;
+let currentUser = null;
+const roomChatMessages = new Map();
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+
+  if (data.rooms) {
+    updateRoomList(data.rooms);
+  } else if (data.users) {
+    updateUserList(data.users);
+  } else if (data.type === 'chat') {
+    displayChatMessage(data.username, data.message);
+  }
+};
+
+function createRoom() {
+  const roomName = prompt('Enter the new room name:');
+  if (roomName) {
+    socket.send(JSON.stringify({ type: 'createRoom', room: roomName }));
+  }
+}
+
+function setUsername() {
+  const usernameInput = document.getElementById('usernameInput');
+  const username = usernameInput.value.trim();
+  if (username) {
+    currentUser = username;
+    updateRoomTitle();
+    updateOnlineUsers([username]);
+    usernameInput.value = '';
+  }
+}
+
+function updateRoomList(rooms) {
+  const roomList = document.getElementById('roomList');
+  roomList.innerHTML = '';
+  rooms.forEach((room) => {
+    const roomItem = document.createElement('li');
+    roomItem.textContent = room;
+    roomItem.onclick = () => joinRoom(room);
+    roomList.appendChild(roomItem);
+  });
+}
+
+function updateRoomTitle() {
+  const roomTitle = document.getElementById('roomTitle');
+  roomTitle.textContent = currentRoom ? `Room: ${currentRoom}` : 'Select a Room';
+}
+
+function updateUserList(users) {
+  const onlineUsersList = document.getElementById('onlineUsers');
+  onlineUsersList.innerHTML = '';
+  users.forEach((user) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = user;
+    onlineUsersList.appendChild(listItem);
+  });
+}
+
+function joinRoom(roomName) {
+  clearChatMessages();
+  loadChatMessages(roomName)
+  currentRoom = roomName;
+  updateRoomTitle();
+  socket.send(JSON.stringify({ type: 'joinRoom', room: roomName, username: currentUser }));
+}
+function clearChatMessages() {
+  const chatMessages = document.getElementById('chatMessages');
+  chatMessages.innerHTML = '';
+}
+
+function displayChatMessage(username, message) {
+  const chatMessages = document.getElementById('chatMessages');
+  const messageElement = document.createElement('div');
+  messageElement.textContent = `${username}: ${message}`;
+  chatMessages.appendChild(messageElement);
+
+  // Auto-scroll the chat box to show the latest message
+  // Store the chat message in the map for the current room
+  const currentRoom = document.getElementById('roomTitle').textContent.replace('Room: ', '');
+  if (!roomChatMessages.has(currentRoom)) {
+    roomChatMessages.set(currentRoom, []);
+  }
+  roomChatMessages.get(currentRoom).push(messageElement.textContent);
+  //chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+function loadChatMessages(room) {
+  const chatMessages = document.getElementById('chatMessages');
+  chatMessages.innerHTML = '';
+  if (roomChatMessages.has(room)) {
+    roomChatMessages.get(room).forEach((message) => {
+      const messageElement = document.createElement('div');
+      messageElement.textContent = message;
+      chatMessages.appendChild(messageElement);
+    });
+  }
+}
+function sendChatMessage() {
+  const chatInput = document.getElementById('chatInput');
+  const message = chatInput.value.trim();
+  if (message && currentUser && currentRoom) {
+    socket.send(JSON.stringify({ type: 'chat', username: currentUser, message: message }));
+    chatInput.value = '';
+  }
+}
+
+document.getElementById('chatInput').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    sendChatMessage();
+  }
+});
--- server.js
+++ server.js
@@ -1,118 +1,91 @@
-const path = require('path')
-const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-
+const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const rooms = new Map(); // Using a Map to store room names and their online users
+const rooms = new Map();
+const users = new Map();
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     const data = JSON.parse(message);
 
-    if (data.request === 'getRooms') {
-      // If the client requests the list of available rooms, send it to them
-      const roomList = Array.from(rooms.keys());
-      ws.send(JSON.stringify({ rooms: roomList }));
-    } else if (data.request === 'joinRoom') {
-      // Handle joining a room
-      const roomToJoin = data.room;
-      // Remove the user from their current room, if any
-      leaveCurrentRoom(ws);
-      // Add the user to the selected room
-      joinRoom(roomToJoin, ws);
+    if (data.type === 'createRoom') {
+      const roomName = data.room;
+      rooms.set(roomName, new Set());
+      broadcastRooms();
+    } else if (data.type === 'getRooms') {
+      ws.send(JSON.stringify({ rooms: Array.from(rooms.keys()) }));
+    } else if (data.type === 'joinRoom') {
+      const roomName = data.room;
+      const username = data.username;
 
-      // Send the updated list of online users for the selected room
-      broadcastOnlineUsers(roomToJoin);
-    } else if (data.request === 'createRoom') {
-      // Handle creating a new room
-      const newRoomName = data.room;
-      if (!rooms.has(newRoomName)) {
-        rooms.set(newRoomName, []);
-        ws.send(JSON.stringify({ message: `Room "${newRoomName}" created successfully!` }));
-      } else {
-        ws.send(JSON.stringify({ error: `Room "${newRoomName}" already exists.` }));
+      if (rooms.has(roomName)) {
+        rooms.get(roomName).add(username);
+        users.set(username, ws);
+        broadcastUsers(roomName);
+        ws.room = roomName;
+        ws.username = username;
       }
-    } else if (data.username) {
-      // Handle setting and updating usernames
-      const newUsername = data.username;
-
-      // Check if the new username is already in use
-      if (ws.username !== newUsername && isUsernameInUse(newUsername, ws.room)) {
-        // If the new username is in use, send a message to the client indicating the conflict
-        ws.send(JSON.stringify({ error: 'Username is already in use' }));
-      } else {
-        // If the new username is not in use, remove the old username and set the new one
-        ws.username = newUsername;
-        
-        // Broadcast the updated list of online users for the room to all clients
-        broadcastOnlineUsers(ws.room);
+    } else if (data.type === 'chat') {
+      if (ws.room && ws.username) {
+        const roomName = ws.room;
+        broadcastChat(roomName, ws.username, data.message);
       }
     }
   });
 
   ws.on('close', () => {
-    // Remove the user from their current room when they disconnect
-    leaveCurrentRoom(ws);
+    const username = ws.username;
+    if (username) {
+      users.delete(username);
+    }
+    if (ws.room) {
+      const roomName = ws.room;
+      rooms.get(roomName).delete(username);
+      broadcastUsers(roomName);
+    }
   });
 });
 
-function isUsernameInUse(username, room) {
-  if (!room) return false; // No room selected
-
-  const roomUsers = rooms.get(room);
-  if (!roomUsers) return false; // Room doesn't exist
-
-  return roomUsers.some((user) => user.username === username);
-}
-
-function joinRoom(room, ws) {
-  let roomUsers = rooms.get(room);
-
-  if (!roomUsers) {
-    roomUsers = [];
-    rooms.set(room, roomUsers);
-  }
-
-  roomUsers.push(ws);
-  ws.room = room;
-}
-
-function leaveCurrentRoom(ws) {
-  if (ws.room) {
-    const roomUsers = rooms.get(ws.room);
-
-    if (roomUsers) {
-      const index = roomUsers.indexOf(ws);
-      if (index !== -1) {
-        roomUsers.splice(index, 1);
-        broadcastOnlineUsers(ws.room);
-        ws.room = null;
-      }
-    }
-  }
-}
-
-function broadcastOnlineUsers(room) {
-  const roomUsers = rooms.get(room);
-  if (!roomUsers) return;
-
-  const users = roomUsers.map((user) => user.username);
-  
-  roomUsers.forEach((client) => {
+function broadcastRooms() {
+  const roomList = Array.from(rooms.keys());
+  const message = { type: 'rooms', rooms: roomList };
+  wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ users: users, room: room }));
+      client.send(JSON.stringify(message));
     }
   });
 }
 
-app.get("/",(req, res) => {
-	res.sendFile(path.join(__dirname, './index.html'));
-})
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+function broadcastUsers(roomName) {
+  const userList = Array.from(rooms.get(roomName));
+  const message = { type: 'users', users: userList };
+  wss.clients.forEach((client) => {
+    if (client.room === roomName && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
+
+function broadcastChat(roomName, username, message) {
+  const messageData = { type: 'chat', username, message };
+  wss.clients.forEach((client) => {
+    if (client.room === roomName && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(messageData));
+    }
+  });
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
