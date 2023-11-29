d9446569a194f4bb667f724546a1f2d696e59c4e
Julian Briones <118573467+cedricJB07@users.noreply.github.com>
2023-11-28 18:53:23 -0800

    adding functional login
    still needs to remove the forms

--- public/script.js
+++ public/script.js
@@ -22,8 +22,57 @@ socket.onmessage = (event) => {
     console.error(data.error);
   } else if (data.users) {
     updateRoomInfo(data.room, data.users);
+  } else if (data.newmessage) {
+    const chatList = document.getElementById('chat');
+    const chatItem = document.createElement('li');
+    chatItem.textContent = data.username + ": " + data.newmessage;
+    chatList.appendChild(chatItem);
+  } else if (data.request === 'login') {
+    handleLoginResponse(data);
   }
 };
+
+function handleLoginResponse(data) {
+  const errorMessage = data.message;
+
+  if (errorMessage === 'Login successful') {
+    hideLoginForms();
+    console.log('Login Success!');
+  } else {
+    // Display an error message to the user
+    const errorDisplay = document.getElementById('loginError');
+    errorDisplay.textContent = errorMessage;
+    console.error('Login failed:', errorMessage);
+  }
+
+}
+
+function hideLoginForms() {
+  const loginForms = document.querySelectorAll('.login-page .form');
+  loginForms.forEach(form => form.style.display = 'none');
+
+  const chatWebsite = document.querySelector('.chat-website');
+  chatWebsite.style.display = 'block';
+
+  const currentUser = document.getElementById('currentUser');
+  currentUser.textContent = document.getElementById('usernameInput').value;
+}
+
+function setCredentials() {
+  const usernameInput = document.getElementById('usernameLogin');
+  const passwordInput = document.getElementById('passwordLogin');
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  if (username && password) {
+    const data = {
+      request: 'login',
+      username: username,
+      password: password
+    };
+    socket.send(JSON.stringify(data));
+  }
+}
 
 function setUsername() {
   const usernameInput = document.getElementById('usernameInput');
@@ -50,6 +99,18 @@ function createRoom() {
   if (roomName) {
     socket.send(JSON.stringify({ request: 'createRoom', room: roomName }));
     socket.send(JSON.stringify({ request: 'getRooms' }));
+  }
+}
+
+function sendMessage() {
+  const chatInputElement = document.getElementById('chatInput');
+  const chatmessage = chatInputElement.value;
+  if (chatmessage) {
+    const data = {
+      chatmessage: chatmessage
+    };
+    socket.send(JSON.stringify(data));
+    chatInputElement.value = '';
   }
 }
 
